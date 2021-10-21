0ff6470b7af3a17f0e1e8d569cec83cda5eb1095
Mateus Alcantara <mateuscastro.al@gmail.com>
2021-10-21 13:17:08 -0300

    Finish challenge

--- src/database/migrations/1634832676582-CreateOrders.ts
+++ src/database/migrations/1634832676582-CreateOrders.ts
@@ -1,11 +1,31 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
 
 export class CreateOrders1634832676582 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
+      await queryRunner.createTable(new Table({
+        name: 'orders',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            isGenerated: true,
+          },
+          {
+            name: 'name',
+            type: 'varchar',
+          },
+          {
+            name: 'game_id',
+            type: 'varchar'
+          }
+      ]
+      }))
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
+      await queryRunner.dropTable('orders')
     }
 
 }
